e09db89a176a30d8e5a71af53e39db91cd259524
Estêvão Felipe <115885688+TevimF@users.noreply.github.com>
2023-12-22 18:11:58 -0300

    tentativa de colocar animaçoes
    animacoes alteradas de andar e ataque, com alguns bugs e incompleta

--- Dark Rat/Assets/Scripts/Player/PlayerController.cs
+++ Dark Rat/Assets/Scripts/Player/PlayerController.cs
@@ -2,30 +2,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject player;
     public float velocidade = 5f;
+    public Animator player_animation;
     private Rigidbody rb;
+    int ataques=0;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+        player_animation = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         MovimentarJogador();
+        CombateJogador();
+        Debug.Log(player_animation.GetInteger("ataque"));
     }
 
     void MovimentarJogador()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
+        //Debug.Log("Horizontal: " + horizontal + " Vertical: " + vertical);
+        // Chama a função para animar o jogador
+        AnimarJogador(); 
+        // Criar um vetor de movimento para ele andar
         Vector3 movimento = new Vector3(horizontal, 0f, vertical);
         Vector3 velocidadeMovimento = movimento * velocidade;
 
         // Configurar a velocidade do Rigidbody
         rb.velocity = new Vector3(velocidadeMovimento.x, rb.velocity.y, velocidadeMovimento.z);
     }
+    void AnimarJogador()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            player_animation.SetBool("esta_andando", true);
+            //Debug.Log("Andando");
+            if (Input.GetAxis("Horizontal") > 0) // Se o jogador estiver indo para a direita
+            {
+                player.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (Input.GetAxis("Horizontal") < 0) // Se o jogador estiver indo para a esquerda
+            {
+                player.transform.rotation = Quaternion.Euler(0, -180, 0);
+            }
+        }
+        else
+        {
+            //Debug.Log("Parado");
+            player_animation.SetBool("esta_andando", false);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Ataquei");
+            player_animation.SetInteger("ataque", 1);
+        }
+        else
+        {
+            player_animation.SetInteger("ataque", 0);
+        }
+    }
+    void CombateJogador() // vai ficar os codigo de atk
+    {
+        AnimarJogador();
+    }
 }
-
-
