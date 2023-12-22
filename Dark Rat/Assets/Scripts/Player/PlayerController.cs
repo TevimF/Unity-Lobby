7301dad02a3e8e68de244e466ea3378cb2f9acd1
Joshuaozao <143628407+Joshuaozao@users.noreply.github.com>
2023-12-22 15:19:17 -0300

    Movimento Simples
    fei

--- Dark Rat/Assets/Scripts/Player/PlayerController.cs
+++ Dark Rat/Assets/Scripts/Player/PlayerController.cs
@@ -2,57 +2,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    #region Variáveis
-
     public float velocidade = 5f;
-    public float limiteXMin = -9f; // Defina o limite mínimo em X
-    public float limiteXMax = 9f;  // Defina o limite máximo em X
-    public float limiteZMin = -10f; // Defina o limite mínimo em Z
-    public float limiteZMax = 9.5f;  // Defina o limite máximo em Z
 
-    private Rigidbody rb;
-    #endregion
-
-    #region Métodos Unity
-    private void Start()
+    void Update()
     {
-        // Inicializa o Rigidbody
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-        // Movimenta o jogador
-        MoverJogador();
-    }
-    #endregion
-
-    #region Métodos Privados
-
-    private void MoverJogador()
-    {
-        // Lógica para movimentar o jogador com base nos controles
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector3 movimento = new Vector3(horizontal, 0, vertical);
-        transform.Translate(movimento * velocidade * Time.deltaTime);
 
-        // Limita o jogador à área específica
-        LimitarPosicao();
+        Vector3 movimento = new Vector3(horizontal, 0f, vertical);
+        Vector3 velocidadeMovimento = movimento * velocidade * Time.deltaTime;
+
+        transform.Translate(velocidadeMovimento);
     }
 
-    private void LimitarPosicao()
+    void OnCollisionEnter(Collision collision)
     {
-        // Obtém a posição atual do jogador
-        Vector3 posicaoAtual = transform.position;
-
-        // Limita as coordenadas x e z dentro da área específica
-        float xLimitado = Mathf.Clamp(posicaoAtual.x, limiteXMin, limiteXMax);
-        float zLimitado = Mathf.Clamp(posicaoAtual.z, limiteZMin, limiteZMax);
-
-        // Define a nova posição limitada
-        transform.position = new Vector3(xLimitado, posicaoAtual.y, zLimitado);
+        // Se houver uma colisão, ajuste a posição para evitar atravessar a parede
+        if (collision.gameObject.CompareTag("Parede"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
     }
-
-    #endregion
 }
