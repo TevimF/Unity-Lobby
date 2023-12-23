1e4da6764249f2a96f43d56c30b87ea25cd5ecda
Estêvão Felipe <115885688+TevimF@users.noreply.github.com>
2023-12-23 01:14:25 -0300

    golpe duplo e movimentação
    golpe duplo implementado e não pode mais bater andando

--- Dark Rat/Assets/Scripts/Player/PlayerController.cs
+++ Dark Rat/Assets/Scripts/Player/PlayerController.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     public GameObject player;
-    public float velocidade = 5f;
+    public float velocidade;
     public Animator player_animation;
     private Rigidbody rb;
-    int ataques=0;
     bool atacando = false;
 
     void Start()
@@ -17,39 +17,48 @@ public class PlayerController : MonoBehaviour
     }
 
     void  Update()   
-    {
+    {   
+        CombateJogador(); 
+        atacando = player_animation.GetCurrentAnimatorStateInfo(0).IsName("RatAttack") || player_animation.GetCurrentAnimatorStateInfo(0).IsName("RatAttack2");
         MovimentarJogador();
-        CombateJogador();
-        Debug.Log(player_animation.GetInteger("ataque"));
     }
-
+    void CombateJogador() // vai ficar os codigo de atk
+    {   
+          if (atacando && Input.GetButtonDown("Fire1")){
+                player_animation.SetInteger("ataque", 2);
+          }
+          else if (!atacando && Input.GetButtonDown("Fire1")){
+                player_animation.SetInteger("ataque", 1);
+          }
+          else  {
+                player_animation.SetInteger("ataque", 0);
+          }
+    }
+     
     void MovimentarJogador()
-    {
-        if (atacando==false)
-        {
-        velocidade= 0;
+    {   
+        if (atacando){
+            velocidade = 0f;    
         }
-        else velocidade= 5;
-        
+        else{
+            velocidade = 5f;
+        }
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         //Debug.Log("Horizontal: " + horizontal + " Vertical: " + vertical);
-        // Chama a função para animar o jogador
-        AnimarJogador(); 
         // Criar um vetor de movimento para ele andar
         Vector3 movimento = new Vector3(horizontal, 0f, vertical);
         Vector3 velocidadeMovimento = movimento * velocidade;
 
         // Configurar a velocidade do Rigidbody
         rb.velocity = new Vector3(velocidadeMovimento.x, rb.velocity.y, velocidadeMovimento.z);   
-       
+        AnimarJogador(); 
     }
     void AnimarJogador()
     {
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             player_animation.SetBool("esta_andando", true);
-            //Debug.Log("Andando");
             if (Input.GetAxis("Horizontal") > 0) // Se o jogador estiver indo para a direita
             {
                 player.transform.rotation = Quaternion.Euler(0, 0, 0);
@@ -61,22 +70,8 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            //Debug.Log("Parado");
             player_animation.SetBool("esta_andando", false);
         }
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Debug.Log("Ataquei");
-            player_animation.SetInteger("ataque", 1);
-            atacando=true;
-        }
-        else
-        {
-            player_animation.SetInteger("ataque", 0);
-        }
-    }
-    void CombateJogador() // vai ficar os codigo de atk
-    {
-        AnimarJogador();
+       
     }
 }
