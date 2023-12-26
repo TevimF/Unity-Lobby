c5a71654136479aba7a67e96c16e52f0d52e90d5
Estêvão Felipe <115885688+TevimF@users.noreply.github.com>
2023-12-26 12:47:53 -0300

    player correndo
    agora ele pode correr com o left shift

--- Dark Rat/Assets/Scripts/Player/PlayerController.cs
+++ Dark Rat/Assets/Scripts/Player/PlayerController.cs
@@ -4,10 +4,11 @@ public class PlayerController : MonoBehaviour
 {
 
     public GameObject player;
-    public float velocidade;
-    public Animator player_animation;
-    private Rigidbody rb;
+    float velocidade;
+    Animator player_animation;
+    Rigidbody rb;
     bool atacando = false;
+    bool correndo = false;
 
     void Start()
     {
@@ -16,62 +17,89 @@ public class PlayerController : MonoBehaviour
         player_animation = GetComponent<Animator>();
     }
 
-    void  Update()   
-    {   
-        CombateJogador(); 
-        atacando = player_animation.GetCurrentAnimatorStateInfo(0).IsName("RatAttack") || player_animation.GetCurrentAnimatorStateInfo(0).IsName("RatAttack2");
+    void FixedUpdate()
+    {
         MovimentarJogador();
     }
-    void CombateJogador() // vai ficar os codigo de atk
-    {   
-          if (atacando && Input.GetButtonDown("Fire1")){
-                player_animation.SetInteger("ataque", 2);
-          }
-          else if (!atacando && Input.GetButtonDown("Fire1")){
-                player_animation.SetInteger("ataque", 1);
-          }
-          else  {
-                player_animation.SetInteger("ataque", 0);
-          }
+    void Update()
+    {
+        CombateJogador();
+        atacando = player_animation.GetCurrentAnimatorStateInfo(0).IsName("RatAttack") || 
+        player_animation.GetCurrentAnimatorStateInfo(0).IsName("RatAttack2");
     }
-     
-    void MovimentarJogador()
-    {   
-        if (atacando){
-            velocidade = 0f;    
+    void CombateJogador() // vai ficar os codigo de atk
+    {
+        if (atacando && Input.GetButtonDown("Fire1"))
+        {
+            player_animation.SetInteger("ataque", 2);
         }
-        else{
+        else if (!atacando && Input.GetButtonDown("Fire1"))
+        {
+            player_animation.SetInteger("ataque", 1);
+        }
+        else
+        {
+            player_animation.SetInteger("ataque", 0);
+        }
+    }
+
+    void MovimentarJogador()
+    {
+        if (atacando)
+        {
+            velocidade = 0f;
+        }
+        else
+        {
             velocidade = 5f;
         }
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        //Debug.Log("Horizontal: " + horizontal + " Vertical: " + vertical);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {  // correr
+            velocidade = 10f;
+            correndo = true;
+        }
+        else
+        {
+            correndo = false;
+        }
         // Criar um vetor de movimento para ele andar
         Vector3 movimento = new Vector3(horizontal, 0f, vertical);
         Vector3 velocidadeMovimento = movimento * velocidade;
 
         // Configurar a velocidade do Rigidbody
-        rb.velocity = new Vector3(velocidadeMovimento.x, rb.velocity.y, velocidadeMovimento.z);   
-        AnimarJogador(); 
+        rb.velocity = new Vector3(velocidadeMovimento.x, rb.velocity.y, velocidadeMovimento.z);
+        AnimarJogador();
     }
     void AnimarJogador()
     {
         if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             player_animation.SetBool("esta_andando", true);
+
             if (Input.GetAxis("Horizontal") > 0) // Se o jogador estiver indo para a direita
             {
-                player.transform.rotation = Quaternion.Euler(0, 0, 0);
+                //virar sprite
+                transform.eulerAngles = new Vector3(0, 0, 0);
             }
             else if (Input.GetAxis("Horizontal") < 0) // Se o jogador estiver indo para a esquerda
             {
-                player.transform.rotation = Quaternion.Euler(0, -180, 0);
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            if (correndo)
+            {
+                player_animation.SetBool("esta_correndo", true);
+            }
+            else
+            {
+                player_animation.SetBool("esta_correndo", false);
             }
         }
         else
         {
             player_animation.SetBool("esta_andando", false);
         }
-       
     }
 }
